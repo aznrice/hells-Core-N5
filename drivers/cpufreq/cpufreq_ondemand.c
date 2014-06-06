a0a1fb883810e69a268d18e5500f78176f056714
hellsgod <hellsgod@gmx.ch>
2014-06-06 17:13:12 +0200

    cpufreq: ondemand: Tweak some values

--- drivers/cpufreq/cpufreq_ondemand.c
+++ drivers/cpufreq/cpufreq_ondemand.c
@@ -150,8 +150,8 @@ static struct dbs_tuners {
 	.up_threshold_any_cpu_load = DEF_FREQUENCY_UP_THRESHOLD,
 	.ignore_nice = 0,
 	.powersave_bias = 0,
-	.sync_freq = 0,
-	.optimal_freq = 0,
+	.sync_freq = 1036800,
+	.optimal_freq = 1190400,
 };
 
 static inline u64 get_cpu_idle_time_jiffy(unsigned int cpu, u64 *wall)
